9488e8b4221e341ad7fe33a6a477f34837c03017
Takuya Matsuyama <hi@craftz.dog>
2020-03-19 16:37:19 +0900

    feat(*): support v4.7.0

--- tasks/config/tasks.js
+++ tasks/config/tasks.js
@@ -116,15 +116,7 @@ module.exports = {
     },
 
     /* What Browsers to Prefix */
-    prefix: {
-      browsers: [
-        'last 2 versions',
-        '> 1%',
-        'opera 12.1',
-        'bb 10',
-        'android 4'
-      ]
-    },
+    prefix: {},
 
     /* File Renames */
     rename: {
